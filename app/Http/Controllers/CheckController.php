e3f879452d75852cd6251ed6b0de5d57aeef8139
gitwithvaishu <vaishupraba73@gmail.com>
2025-05-30 18:59:50 +0530

    updated

--- app/Http/Controllers/CheckController.php
+++ app/Http/Controllers/CheckController.php
@@ -7,6 +7,7 @@ use App\Models\Check;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\CheckAuthRequest;
+use App\Http\Requests\LoginRequest;
 
 class CheckController extends Controller
 {
@@ -29,5 +30,45 @@ class CheckController extends Controller
                 'token'=>$data->createToken('API Tokens'.$data->email)->plainTextToken,
             ]
         );
+    }
+
+    public function login(LoginRequest $request){
+        $authenticated = $request->validated();
+        $user = Check::where('email',$request->email)->first();
+        /* if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json(
+                [
+                    'Status'=>0,
+                    'Message'=>'Invalid credentials'
+                ]
+            );
+        }
+        return response()->json(
+            [
+                'Status'=>1,
+                'Message'=>'Login successfully',
+                'token'=>$user->createToken('API Token'.$user->email)->plainTextToken
+            ]
+        ); */
+
+
+        if(Hash::check($request->password,$user->password )&& ($user->email==$request->email)){
+            return response()->json(
+            [
+                'Status'=>1,
+                'Message'=>'Login successfully',
+                'token'=>$user->createToken('API Token'.$user->email)->plainTextToken
+            ]
+        );
+        }
+
+        else{
+            return response()->json(
+                [
+                    'Status'=>0,
+                    'Message'=>'Invalid credentials'
+                ]
+            );
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/register',[CheckController::class,'register']);
+Route::post('/login',[CheckController::class,'login']);
